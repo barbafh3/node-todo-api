f5f2173da7cd915a81346c1d3a3d9375a1138166
Gilberto Timotheo Junior <barbafh@gmail.com>
2018-10-26 21:44:59 -0300

    Added GET /todos/:id tests still with errors

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,19 +1,23 @@
 const expect = require('expect');
 const request = require('supertest');
 
+const {ObjectID} = require('mongodb');
+
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+    _id: new ObjectID(),
     text: 'First test todo'
 }, {
+    _id: new ObjectID(),
     text: 'Second test todo'
-}]
+}];
 
 beforeEach((done) => {
     Todo.remove({}).then(() => {
-       return Todo.insertMany(todos)
-    }).then(() =>  done());
+       return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -68,3 +72,15 @@ describe('GET /todos', () => {
             .end(done());
     })
 });
+
+describe('GET /todos/:id', () => {
+    it('should return todo doc', (done) => {
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(() => {done();});
+    });
+})
