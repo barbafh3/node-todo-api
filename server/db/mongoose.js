c4b28120199d12fa6a3ead8d1cf98516337a39e9
Gilberto Timotheo Junior <barbafh@gmail.com>
2018-10-28 22:22:34 -0300

    Added configuration for mLab support when using heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+let db = {
+		localhost: 'mongodb://localhost:27017/TodoApp',
+		mlab: 'mongodb://root:mestre123@ds143953.mlab.com:43953/todoapp'
+}
+mongoose.connect( db.localhost || db.mlab, { useNewUrlParser: true });
 
 module.exports = {mongoose};
