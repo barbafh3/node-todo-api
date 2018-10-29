1848c7cb679eea0fa0e6d921b58c40f13d43fe6b
Gilberto Timotheo Junior <barbafh@gmail.com>
2018-10-28 23:04:21 -0300

    Fixed mongoose.js db variable

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,15 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-let db = {
+var db = {
 		localhost: 'mongodb://localhost:27017/TodoApp',
 		mlab: 'mongodb://root:mestre123@ds143953.mlab.com:43953/todoapp'
 }
-mongoose.connect( db.localhost || db.mlab, { useNewUrlParser: true });
+mongoose.connect( db.localhost || db.mlab, { useNewUrlParser: true })
+		.then(() => {
+    		console.log("CONNECTED TO DATABASE")
+		},(e)=>{
+        	console.log(e);
+		});
 
 module.exports = {mongoose};
