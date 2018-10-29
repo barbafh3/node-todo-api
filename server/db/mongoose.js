e442851a458c30d008406c7cac0ec865d3d91e0d
Gilberto Timotheo Junior <barbafh@gmail.com>
2018-10-28 23:16:18 -0300

    Another try on fixing mlab connection error

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,7 @@ var db = {
 		localhost: 'mongodb://localhost:27017/TodoApp',
 		mlab: process.env.MONGODB_URI
 }
-mongoose.connect( db.localhost || db.mlab, { useNewUrlParser: true })
+mongoose.connect( db.mlab || db.localhost, { useNewUrlParser: true })
 		.then(() => {
     		console.log("CONNECTED TO DATABASE")
 		},(e)=>{
