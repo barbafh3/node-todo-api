46512857023b40ed4a985f9592e0826c4e6134d9
Gilberto Timotheo Junior <barbafh@gmail.com>
2018-10-31 00:11:33 -0300

    Added update function PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo');
@@ -45,6 +45,30 @@ app.get('/todos/:id', (req, res) => {
             res.status(400).send(e);
         })
     }
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime()
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
+        .then((todo) => {
+            if (!todo) {
+                return res.status(404).send();
+            }
+            res.send({todo});
+        }).catch((e) => done(e));
 });
 
 app.delete('/todos/:id', (req, res) => {
