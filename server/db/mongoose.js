0e6daa9fd83d23de40cf8b3c7cd35a41eefb0318
Gilberto Timotheo Junior <barbafh@gmail.com>
2018-10-31 11:00:18 -0300

    Added environment control for dev/test/production

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-var db = {
-		localhost: 'mongodb://localhost:27017/TodoApp',
-		mlab: process.env.MONGODB_URI
-}
-mongoose.connect( db.mlab || db.localhost, { useNewUrlParser: true })
+mongoose.connect( process.env.MONGODB_URI, { useNewUrlParser: true })
 		.then(() => {
     		console.log("CONNECTED TO DATABASE")
 		},(e)=>{
