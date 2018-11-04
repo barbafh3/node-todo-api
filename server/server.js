112e33c6abde24bef14acffc483e47c6e884285d
Gilberto Timotheo Junior <barbafh@gmail.com>
2018-11-03 22:13:14 -0300

    Added POST /users/login route

--- server/server.js
+++ server/server.js
@@ -27,7 +27,8 @@ app.post('/todos', (req, res) => {
 });
 
 app.post('/users', (req, res) => {
-    var user = new User(_.pick(req.body, ['email', 'password']));
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
 
     user.save().then(() => {
         return user.generateAuthToken();
@@ -38,6 +39,18 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        })
+    }).catch((e) => {
+        res.status(400).send();
+    });
 });
 
 app.get('/todos', (req, res) => {
